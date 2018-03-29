2a88670cf9fe21d41f7ffd3a45951b408edf4627
dibosh <dibosh@newscred.com>
2018-03-30 00:23:44 +0600

    Add necessary documentations

--- src/clients/omdb.client.js
+++ src/clients/omdb.client.js
@@ -24,6 +24,19 @@ class OMDBAPIClient extends BaseClient{
     };
   }
 
+  /**
+   * Get a movie by looking up using different params (e.g. title, imdb id, year etc.)
+   * @param options
+   * Example- {
+   *  title: 'Saw',
+   *  year: 2004,
+   *  plot: 'full|short',
+   *  format: 'json|xml',
+   *  type: 'movie|series',
+   *  imdbID: tt12444
+   * }
+   * @returns {*|Promise.<TResult>}
+   */
   get(options) {
     let reqOptions = this._translateIncomingRequestOptions(options);
     return this._makeHTTPGET(this.authParam, reqOptions, null, this._transformResponse)
@@ -32,6 +45,17 @@ class OMDBAPIClient extends BaseClient{
       });
   }
 
+  /**
+   * Search for a movie.
+   * @param options
+   * Example- {
+   *  query: 'Saw',
+   *  year: 2004,
+   *  format: 'json|xml',
+   *  type: 'movie|series|episode'
+   * }
+   * @returns {*|Promise.<TResult>}
+   */
   search(options) {
     let reqOptions = this._translateIncomingRequestOptions(options);
     return this._makeHTTPGET(this.authParam, reqOptions, null, this._transformResponse)
@@ -40,10 +64,22 @@ class OMDBAPIClient extends BaseClient{
       });
   }
 
+  /**
+   * Get a movie by title and/or year
+   * @param title Title of the movie
+   * @param year Optional
+   * @param type Optional. Default- movie
+   * @returns {*|Promise.<TResult>}
+   */
   getByTitleAndYear(title, year, type = 'movie') {
     return this.get({title: title, year: year, format: 'json', plot: 'short', type: type});
   }
 
+  /**
+   * Get a movie by IMDB ID
+   * @param imdbID
+   * @returns {*|Promise.<TResult>}
+   */
   getByIMDBId(imdbID) {
     return this.get({imdbID: imdbID, format: 'json', plot: 'short'});
   }
