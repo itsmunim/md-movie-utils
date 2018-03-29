8ff792866d47753ced40de43b4a9eb5744668d2c
dibosh <dibosh@newscred.com>
2018-03-30 00:21:05 +0600

    Base client must be as lean as possible

--- src/clients/base.client.js
+++ src/clients/base.client.js
@@ -29,14 +29,6 @@ class BaseClient {
   /**
    * Get a movie by looking up using different params (e.g. title, imdb id, year etc.)
    * @param options
-   * Example- {
-   *  title: 'Saw',
-   *  year: 2004,
-   *  plot: 'full|short',
-   *  format: 'json|xml',
-   *  type: 'movie|series',
-   *  imdbID: tt12444
-   * }
    */
   get(options) {
     throw new Error('Not implemented');
@@ -45,32 +37,8 @@ class BaseClient {
   /**
    * Search for a movie.
    * @param options
-   * Example- {
-   *  query: 'Saw',
-   *  year: 2004,
-   *  format: 'json|xml',
-   *  type: 'movie|series|episode'
-   * }
    */
   search(options) {
-    throw new Error('Not implemented');
-  }
-
-  /**
-   * Get a movie by title and/or year
-   * @param title Title of the movie
-   * @param year Optional
-   * @param type Optional. Default- movie
-   */
-  getByTitleAndYear(title, year, type = 'movie') {
-    throw new Error('Not implemented');
-  }
-
-  /**
-   * Get a movie by IMDB ID
-   * @param imdbID
-   */
-  getByIMDBId(imdbID) {
     throw new Error('Not implemented');
   }
 
